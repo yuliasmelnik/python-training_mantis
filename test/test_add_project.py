a525ffaebb3a5cc2977d1fc1c0874ec44cb0f5f7
Melnik <jusapeshkina@gmail.com>
2025-08-13 10:25:11 +0300

    Задание 26

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -4,12 +4,12 @@ import json
 
 def test_add_project( app, xlsx_projects, db):
     project = xlsx_projects
-    old_projects = db.get_project_list()
-    app.project.create(project)
     with open('target.json') as file:
         data = json.load(file)
         username = data['webadmin']['username']
         password = data['webadmin']['password']
+    old_projects = app.soap.get_project_list(username=username, password=password)
+    app.project.create(project)
     new_projects = app.soap.get_project_list(username=username, password=password)
     old_projects.append(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -4,15 +4,15 @@ from model.project import Project
 
 
 def test_del_project(app, db):
-    if len(db.get_project_list()) == 0:
-        app.project.create(Project(name="test", status="10", description="test"))
-    old_projects = db.get_project_list()
-    project = random.choice(old_projects)
-    app.project.delete_project_by_id(project.id)
     with open('target.json') as file:
         data = json.load(file)
         username = data['webadmin']['username']
         password = data['webadmin']['password']
+    if len(app.soap.get_project_list(username=username, password=password)) == 0:
+        app.project.create(Project(name="test", status="10", description="test"))
+    old_projects = app.soap.get_project_list(username=username, password=password)
+    project = random.choice(old_projects)
+    app.project.delete_project_by_id(project.id)
     new_projects = app.soap.get_project_list(username=username, password=password)
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
