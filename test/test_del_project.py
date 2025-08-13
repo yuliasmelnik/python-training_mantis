f10941d18a58882b729cc05656d127a12913e6e4
Melnik <jusapeshkina@gmail.com>
2025-08-13 10:12:37 +0300

    Задание 26

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -1,4 +1,5 @@
 import random
+import json
 from model.project import Project
 
 
@@ -8,6 +9,10 @@ def test_del_project(app, db):
     old_projects = db.get_project_list()
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.id)
-    new_projects = db.get_project_list()
+    with open('target.json') as file:
+        data = json.load(file)
+        username = data['webadmin']['username']
+        password = data['webadmin']['password']
+    new_projects = app.soap.get_project_list(username=username, password=password)
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
