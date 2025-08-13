a58a70aec26698d6c5cf93aa023dabcf9704ab22
Melnik <jusapeshkina@gmail.com>
2025-08-13 10:10:09 +0300

    Задание 26 (тест на добавление проектов)

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,15 @@
 from model.project import Project
+import json
 
 
 def test_add_project( app, xlsx_projects, db):
     project = xlsx_projects
     old_projects = db.get_project_list()
     app.project.create(project)
-    new_projects = app.soap.get_project_list(username="administrator", password ="root")
+    with open('target.json') as file:
+        data = json.load(file)
+        username = data['webadmin']['username']
+        password = data['webadmin']['password']
+    new_projects = app.soap.get_project_list(username=username, password=password)
     old_projects.append(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
