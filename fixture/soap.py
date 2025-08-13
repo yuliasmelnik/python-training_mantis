1adb808766340a9765416229b3d61b254dc6a41d
Melnik <jusapeshkina@gmail.com>
2025-08-13 07:12:05 +0300

    Задание 26 (промежуточное тест на добавление проектов)

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,6 +1,9 @@
 from suds.client import Client
 from suds import WebFault
 
+from model.project import Project
+
+
 class SoapHelper:
 
     def __init__(self, app):
@@ -15,10 +18,17 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
+        list = []
         client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
         try:
             result = client.service.mc_projects_get_user_accessible(username, password)
-            return result
+            for element in result:
+                id = element.id
+                name = element.name
+                status = element.status.id
+                description = element.description
+                list.append(Project(name=str(name), status=int(status), description=str(description), id=str(id)))
+            return list
         except WebFault:
             return False
 
