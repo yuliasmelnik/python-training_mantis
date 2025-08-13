817eafc96c4671f3c4788c01fb964d36f1242e2e
Melnik <jusapeshkina@gmail.com>
2025-08-13 12:30:33 +0300

    Задание 26(soap.py добавлено использование base_url)

--- fixture/soap.py
+++ fixture/soap.py
@@ -10,7 +10,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -19,7 +19,7 @@ class SoapHelper:
 
     def get_project_list(self, username, password):
         list = []
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             result = client.service.mc_projects_get_user_accessible(username, password)
             for element in result:
